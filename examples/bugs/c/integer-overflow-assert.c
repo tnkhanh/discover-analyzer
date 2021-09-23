2b9efd14f8aa69c4fcf6f5174e9ff37da186faba
trungtq <taquangtrungvn@gmail.com>
2021-09-23 18:55:10 +0800

    update examples

--- examples/bugs/c/integer-overflow-assert.c
+++ examples/bugs/c/integer-overflow-assert.c
@@ -8,8 +8,10 @@ int main(int argc, char** argv) {
   scanf("%d", &a);
 
   int x = a * 4;
+  __assert_integer_overflow(&x, sizeof(x));
 
   long y = a * 10;
+  __assert_no_integer_overflow(&y, sizeof(y));
 
   printf("x: %d\n", x);
   printf("y: %lu\n", y);
