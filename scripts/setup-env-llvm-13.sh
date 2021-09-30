c697fb736ef3eb47223bf347b088d35be7d6dd15
trungtq <taquangtrungvn@gmail.com>
2021-09-30 23:28:19 +0800

    update clang path

---
+++ scripts/setup-env-llvm-13.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Source this file to update the environment variables.
+
+export PATH=$HOME/llvm/llvm-13/bin:$PATH
+export LD_LIBRARY_PATH=$HOME/llvm/llvm-13/lib:$LD_LIBRARY_PATH
+export LIBRARY_PATH=$HOME/llvm/llvm-13/lib:$LIBRARY_PATH
