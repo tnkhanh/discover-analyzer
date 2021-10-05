23a0233701244d863eec9f1ba5b6f0361137fa9a
TN Khanh <tnkhanh4@gmail.com>
2021-10-05 10:13:38 +0000

    Code style fix

--- src/llvm/llinstrument.ml
+++ src/llvm/llinstrument.ml
@@ -18,14 +18,6 @@ module SP = Set.Poly
 module LP = Llloop
 module LG = Llcfg
 
-
-let instrument_bug_annotation (modul: LL.llmodule) : unit =
-  (* TODO: fill code here.
-     See module llsimp.ml, function elim_instr_intrinsic_lifetime ...
-     for how to manipulating LLVM bitcode *)
-  let _ = print "INSTRUMENT BUG ANNOTATION" in
-  ()
-
 let extract_annotations (filename: string) = 
   let _ = print_endline ("File:.........."^filename) in
   let inchan = 
@@ -40,27 +32,23 @@ let extract_annotations (filename: string) =
           (*NOTE: no asterisk allowed in Bug string*)
             Str.search_forward
             (Str.regexp "/\\*[ \t]*{[ \t]*Bug:\\([^*]*\\)\\*/") line col_number
-          with
-          | Not_found -> -1 in
-
+          with Not_found -> -1 in
         if match_pos = -1 then old_list
         else
           find_annot (Str.match_end ())
             ((line_number, (Str.match_end ()) + 1, Str.matched_group 1 line)::old_list) in
-
       let new_list = find_annot 0 annot_list in
       read_line (line_number+1) new_list
-    with
-    | End_of_file -> annot_list in
+    with End_of_file -> annot_list in
   read_line 1 []
 
+let instrument_bug_annotation (modul: LL.llmodule) : unit =
+  (* TODO: fill code here.
+     See module llsimp.ml, function elim_instr_intrinsic_lifetime ...
+     for how to manipulating LLVM bitcode *)
+  let _ = print "INSTRUMENT BUG ANNOTATION" in
+
+  ()
+
 let instrument_bitcode filename (modul: LL.llmodule) : unit =
-  let annotations = extract_annotations 
-    ((String.sub filename ~pos:0 ~len:(String.length filename - 11))^"c") in
-  let _ = print_endline "Annotations: " in
-  let _ = 
-    List.iter 
-      ~f:(fun (x, y, s) -> print_endline ((string_of_int x)^" "^(string_of_int y)^" "^s))
-      annotations in
- 
   instrument_bug_annotation modul
