2b3edb5ef4065794fde4e9cb57556d55444167be
trungtq <taquangtrungvn@gmail.com>
2021-11-09 16:01:43 +0800

    empty interface for Discover to allow ocamlc report warnings

---
+++ src/frontend/discover.mli
@@ -0,0 +1,11 @@
+(********************************************************************
+ * This file is part of the source code analyzer Discover.
+ *
+ * Copyright (c) 2020-2021 Singapore Blockchain Innovation Programme.
+ * All rights reserved.
+ ********************************************************************)
+
+(* IMPORTANT: do not delete this file.
+
+   It's kept as an empty file so that the OCaml compiler can report warnings
+   about unused module aliases *)
