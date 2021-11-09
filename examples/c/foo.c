e4cdf3bdab25be303aad6734c911039538c3b5c9
trungtq <taquangtrungvn@gmail.com>
2021-11-09 11:23:44 +0800

    example

---
+++ examples/c/foo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+// #include <discover.h>
+
+int main(int argc, char** argv) {
+  int a = 1;
+
+  printf("Input an integer: ");
+  scanf("%d", &a);
+
+  if (a > 10) a = 1234567890;
+  else a = 1234567891;
+  long x = /*@{Bug:IntegerOverflow*/ a * 10 /*@:Bug}*/;
+
+  printf("Input an integer: ");
+  scanf("%d", &a);
+
+  if (a > 10) a = 123456;
+  else a = 123457;
+  /*@{Bug:IntegerOverflow*/ short y = a * 10 /*@:Bug}*/;
+
+  long z = x+y;
+
+  return z;
+}
