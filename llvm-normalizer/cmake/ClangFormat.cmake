39eb406918b7170a049edfb37883d8ed427e5cb5
Ta Quang Trung <taquangtrungvn@gmail.com>
2021-11-24 11:54:46 +0800

    [llvm-normalizer] Cmake file for clangformat

---
+++ llvm-normalizer/cmake/ClangFormat.cmake
@@ -0,0 +1,22 @@
+####################################################################
+# This file is part of the tool Normalizer of the project Discover.
+#
+# Copyright (c) 2020-2021 Singapore Blockchain Innovation Programme.
+# All rights reserved.
+####################################################################
+
+file(GLOB_RECURSE SOURCE_FILES
+  ${PROJECT_SOURCE_DIR}/src/*.cpp
+  ${PROJECT_SOURCE_DIR}/src/*.h)
+
+# # debugging
+# foreach (FILE ${SOURCE_FILES})
+#   message (STATUS "Source file: ${FILE}")
+# endforeach ()
+
+add_custom_target(
+  clangformat
+  COMMAND clang-format
+  -style=LLVM
+  -i
+  ${SOURCE_FILES})
