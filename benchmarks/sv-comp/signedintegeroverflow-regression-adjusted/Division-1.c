8200ee09e3d5e4785c156f8477b7874aeaba73a0
TN Khanh <tnkhanh4@gmail.com>
2021-11-25 15:52:40 +0800

    Svcomp benchmarks fix (#51)
    * Fix underflow typo
    
    * Add adjusted benchmarks to avoid constant folding

---
+++ benchmarks/sv-comp/signedintegeroverflow-regression-adjusted/Division-1.c
@@ -0,0 +1,14 @@
+// Author: heizmann@informatik.uni-freiburg.de
+// Date: 2015-09-01
+//
+// We assume sizeof(int)=4.
+
+#include <stdio.h>
+//#include "discover.h"
+
+int main() {
+	int minInt = -2147483647 - 1;
+	int x = /*@{Bug:IntegerOverflow*/(minInt / -1 )/*@:Bug}*/ - 1;
+	printf("%d\n", x);
+	return 0;
+}
