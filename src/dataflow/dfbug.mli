f3b4189e3ab47bcd44921e388042e998a296f562
Ta Quang Trung <taquangtrungvn@gmail.com>
2021-11-29 19:10:59 +0800

    add interface mli files

---
+++ src/dataflow/dfbug.mli
@@ -0,0 +1,8 @@
+(********************************************************************
+ * This file is part of the source code analyzer Discover.
+ *
+ * Copyright (c) 2020-2021 Singapore Blockchain Innovation Programme.
+ * All rights reserved.
+ ********************************************************************)
+
+val find_bugs : Dfcore.program_data -> unit
