d1654fba8cf8bb2a196d562c26a4f40ae9aa9947
TN Khanh <tnkhanh4@gmail.com>
2021-12-16 10:13:59 +0000

    Parse yaml files and run discover

--- src/benchmark/benchmark.ml
+++ src/benchmark/benchmark.ml
@@ -1,5 +1,6 @@
 open Dcore
 module PS = Process
+module EJ = Ezjsonm
 
 let _ = print "Hello!\n"
 let dir_prefix = "benchmarks/"
@@ -10,17 +11,35 @@ let test benchmark =
   let full_dir = dir_prefix ^ dir in
   let _ = print ("Testing " ^ description ^ " in " ^ dir) in
   let config_filename = full_dir ^ "/benchmark.yaml" in
-  let config_str = In_channel.read_all config_filename in
-  let config_value = Yaml.of_string_exn config_str in
-  print (Yaml.to_string_exn config_value)
-(*  let output = PS.run_command_get_output ["./discover"; "--clang-option=-I lib/discover";
-                  "--dfa-pointer"; 
-                  "benchmarks/ptaben/ptaben-updated/basic_c_tests/global-array.c"; "-d"] in
+  let configs_str = In_channel.read_all config_filename in
+  let configs_value = Yaml.of_string_exn configs_str in
+  let configs_list = EJ.get_dict configs_value in
+  List.iter configs_list ~f:(fun (name, config) ->
+    let clang_option = EJ.get_string (EJ.find config ["clang-option"]) in
+    let discover_option = EJ.get_string (EJ.find config ["discover-option"]) in
+    let targets = EJ.get_string (EJ.find config ["targets"]) in
+    let _ = print ("\n" ^ name ^ ":________") in
+    let _ = print (Yaml.to_string_exn config) in
+    let files = 
+      let ls_output = PS.run_command_get_output (["ls"; full_dir ^"/"^targets]) in
+      match ls_output with
+      | Ok ls_output_str -> let _ = print ls_output_str in Str.split (Str.regexp "[ \n\r\x0c\t]+") ls_output_str
+      | Error msg -> let _ = print msg in [] in
 
+    List.iter files ~f:(fun file ->
+      let output = PS.run_command_get_output 
+                     (["./discover"; 
+                      "--clang-option=" ^ clang_option]
+                      @ (String.split_on_chars ~on:[' '] discover_option)
+                      @ [full_dir ^ "/" ^ file]) in
 
-  match output with
-  | Ok result -> print ("Output: " )
-  | Error msg -> print ("Error messsage: ") *)
+      let output_str = match output with
+      | Ok result -> result
+      | Error msg -> msg in
+      let full_log_dir = log_dir ^ dir in
+      Out_channel.write_all (full_log_dir ^ "/" ^ file) ~data:output_str
+    )
+  )
 
 let benchmarks = [
   ("PTABEN", "ptaben/ptaben-updated/basic_c_tests")]
