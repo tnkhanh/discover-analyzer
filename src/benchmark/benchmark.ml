2a7012b9b80cc8e560e8146370805db71f4e37bd
TN Khanh <tnkhanh4@gmail.com>
2021-12-17 03:33:36 +0000

    Test run discover with parameters

--- src/benchmark/benchmark.ml
+++ src/benchmark/benchmark.ml
@@ -17,29 +17,47 @@ let test benchmark =
   List.iter configs_list ~f:(fun (name, config) ->
     let clang_option = EJ.get_string (EJ.find config ["clang-option"]) in
     let discover_option = EJ.get_string (EJ.find config ["discover-option"]) in
-    let targets = EJ.get_string (EJ.find config ["targets"]) in
+    let full_log_dir = log_dir ^ dir ^ "/" ^ name in
+    let _ = PS.run_command ["mkdir"; "-p"; full_log_dir] in
+
+    (*let targets = EJ.get_string (EJ.find config ["targets"]) in*)
     let _ = print ("\n" ^ name ^ ":________") in
-    let _ = print (Yaml.to_string_exn config) in
+    let _ = print ("....." ^ (EJ.value_to_string config)) in
     let files = 
-      let ls_output = PS.run_command_get_output (["ls"; full_dir ^"/"^targets]) in
-      match ls_output with
-      | Ok ls_output_str -> let _ = print ls_output_str in Str.split (Str.regexp "[ \n\r\x0c\t]+") ls_output_str
-      | Error msg -> let _ = print msg in [] in
+(*      let ls_output = PS.run_command_get_output ["ls"; full_dir^"/*.c" [>^"/"^targets <]] in*)
+      (*match ls_output with*)
+      (*| Ok ls_output_str -> let _ = print ls_output_str in Str.split (Str.regexp "[ \n\r\x0c\t]+") ls_output_str*)
+      (*| Error msg -> let _ = print msg in [] in*)
+      (*FileUtil.ls full_dir in*)
+      Array.to_list (Sys.readdir full_dir) in
 
     List.iter files ~f:(fun file ->
-      let output = PS.run_command_get_output 
-                     (["./discover"; 
+      let full_filepath = full_dir ^ "/" ^ file in
+      let _ = print ("File: " ^ full_filepath) in
+      let command = ["./discover"; 
                       "--clang-option=" ^ clang_option]
                       @ (String.split_on_chars ~on:[' '] discover_option)
-                      @ [full_dir ^ "/" ^ file]) in
+                      @ [full_filepath] in
+      let _ = List.iter command ~f:(fun str -> print ("Comm: " ^ str)) in
+      let output = PS.run_command_get_output command in
 
       let output_str = match output with
       | Ok result -> result
       | Error msg -> msg in
-      let full_log_dir = log_dir ^ dir in
-      Out_channel.write_all (full_log_dir ^ "/" ^ file) ~data:output_str
+      let log_file = full_log_dir ^ "/" ^ file^".log" in
+      let _ = print ("Log: " ^ log_file) in
+      Out_channel.write_all log_file ~data:output_str
     )
   )
+;;
+
+let _ =
+  let ls_test = PS.run_command_get_output ["ls"; "*.c"] in
+  let ls_str = match ls_test with
+    | Ok str -> str
+    | Error str -> str in
+  print ("ls:...\n" ^ ls_str)
+;;
 
 let benchmarks = [
   ("PTABEN", "ptaben/ptaben-updated/basic_c_tests")]
