6f924687c0831f71f7c87ba8e07e15b28065f7cc
TN Khanh <tnkhanh4@gmail.com>
2021-12-19 09:24:50 +0000

    Clean up code

--- src/benchmark/benchmark.ml
+++ src/benchmark/benchmark.ml
@@ -2,7 +2,6 @@ open Dcore
 module PS = Process
 module EJ = Ezjsonm
 
-let _ = print "Hello!\n"
 let dir_prefix = "benchmarks/"
 let log_dir = "benchmark-log/"
 
@@ -15,59 +14,39 @@ let test benchmark =
   let configs_value = Yaml.of_string_exn configs_str in
   let configs_list = EJ.get_dict configs_value in
   List.iter configs_list ~f:(fun (name, config) ->
-    let clang_option = EJ.get_string (EJ.find config ["clang-option"]) in
-    let discover_option = EJ.get_string (EJ.find config ["discover-option"]) in
-    let full_log_dir = log_dir ^ dir ^ "/" ^ name in
-    let _ = PS.run_command ["mkdir"; "-p"; full_log_dir] in
+      let clang_option = EJ.get_string (EJ.find config [ "clang-option" ]) in
+      let discover_option =
+        EJ.get_string (EJ.find config [ "discover-option" ]) in
+      let full_log_dir = log_dir ^ dir ^ "/" ^ name in
+      let _ = PS.run_command [ "mkdir"; "-p"; full_log_dir ] in
+      let _ = print ("\n" ^ name ^ ":________") in
+      let _ = print ("....." ^ EJ.value_to_string config) in
+      let all_files = Array.to_list (Sys.readdir full_dir) in
+      let targets =
+        String.split ~on:' ' (EJ.get_string (EJ.find config [ "targets" ]))
+      in
+      let _ =
+        List.iter targets ~f:(fun target -> print ("Target: " ^ target)) in
+      (* TODO: Check if a file is a target *)
+      let is_target target_list file = true in
+      let files = List.filter all_files ~f:(is_target targets) in
+      List.iter files ~f:(fun file ->
+          let full_filepath = full_dir ^ "/" ^ file in
+          let _ = print ("File: " ^ file) in
+          let command =
+            [ "./discover"; "--clang-option=" ^ clang_option ]
+            @ String.split ~on:' ' discover_option
+            @ [ full_filepath ] in
+          (*let _ = List.iter command ~f:(fun str -> print ("Comm: " ^ str)) in*)
+          let output = PS.run_command_get_output command in
 
-    (*let targets = EJ.get_string (EJ.find config ["targets"]) in*)
-    let _ = print ("\n" ^ name ^ ":________") in
-    let _ = print ("....." ^ (EJ.value_to_string config)) in
-    let all_files = 
-(*      let ls_output = PS.run_command_get_output ["ls"; full_dir^"/*.c" [>^"/"^targets <]] in*)
-      (*match ls_output with*)
-      (*| Ok ls_output_str -> let _ = print ls_output_str in Str.split (Str.regexp "[ \n\r\x0c\t]+") ls_output_str*)
-      (*| Error msg -> let _ = print msg in [] in*)
-      (*FileUtil.ls full_dir in*)
-      Array.to_list (Sys.readdir full_dir) in
-    let targets = String.split ~on: ' ' (EJ.get_string (EJ.find config ["targets"])) in
-    let _ = List.iter targets ~f:(fun target -> print ("Target: " ^ target)) in
-    let is_target target_list file = true in
-(*      match target_list with*)
-      (*| [] -> false*)
-      (*| target :: tl -> *)
-        (*if Str.string_match (Str.regexp target) file 0 then true*)
-        (*else*)
-          (*is_target tl file in*)
-    let files = List.filter all_files ~f:(is_target targets) in
-    List.iter files ~f:(fun file ->
-      let full_filepath = full_dir ^ "/" ^ file in
-      let _ = print ("File: " ^ file) in
-      let command = ["./discover"; 
-                      "--clang-option=" ^ clang_option]
-                      @ (String.split ~on:' ' discover_option)
-                      @ [full_filepath] in
-      (*let _ = List.iter command ~f:(fun str -> print ("Comm: " ^ str)) in*)
-      let output = PS.run_command_get_output command in
-
-      let output_str = match output with
-      | Ok result -> result
-      | Error msg -> msg in
-      let log_file = full_log_dir ^ "/" ^ file^".log" in
-      (*let _ = print ("Log: " ^ log_file) in*)
-      Out_channel.write_all log_file ~data:output_str
-    )
-  )
+          let output_str =
+            match output with
+            | Ok result -> result
+            | Error msg -> msg in
+          let log_file = full_log_dir ^ "/" ^ file ^ ".log" in
+          Out_channel.write_all log_file ~data:output_str))
 ;;
 
-let _ =
-  let ls_test = PS.run_command_get_output ["ls"] in
-  let ls_str = match ls_test with
-    | Ok str -> str
-    | Error str -> str in
-  print ("ls:...\n" ^ ls_str)
-;;
-
-let benchmarks = [
-  ("PTABEN", "ptaben/ptaben-updated/basic_c_tests")]
+let benchmarks = [ "PTABEN", "ptaben/ptaben-updated/basic_c_tests" ]
 let _ = List.iter benchmarks ~f:test
