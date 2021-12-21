4630764732fb1410262293bfdcefbe3fa049ded2
TN Khanh <tnkhanh4@gmail.com>
2021-12-21 02:51:15 +0000

    Refactor code

--- src/test/test_benchmark.ml
+++ src/test/test_benchmark.ml
@@ -10,7 +10,7 @@ type config = {
   conf_recurse: bool;
 }
 
-let mk_default_config () =
+let default_config =
   {
     conf_name = "default"; 
     conf_clang_opt = "";
@@ -29,33 +29,42 @@ let mk_config (name: string) (clang_opt: string) (dis_opt: string) (recurse: boo
   }
 
 let read_confs_from_file filename =
-  try
-    let configs_str = In_channel.read_all filename in
-    let configs_value = Yaml.of_string_exn configs_str in
-    let configs_list = EJ.get_dict configs_value in
-    List.map configs_list ~f:(fun (name, conf_yaml) ->
-      let clang_option = EJ.get_string (EJ.find conf_yaml [ "clang-option" ]) in
-      let discover_option =
-        EJ.get_string (EJ.find conf_yaml [ "discover-option" ]) in
-      let recurse_str = 
-        EJ.get_string (EJ.find conf_yaml [ "recursive" ]) in
-      let recurse =
-        if String.equal recurse_str "true" then true else false in
-      let targets =
-        String.split ~on:' ' (EJ.get_string (EJ.find conf_yaml [ "targets" ])) in
+  let _ = print filename in
+  let configs_str = 
+    try In_channel.read_all filename 
+    with e -> let _ = print ("Exn: " ^ Exn.to_string e) in "" in
+  if String.equal configs_str "" then [default_config]
+  else
+  let _ = print configs_str in
+  let configs_value = Yaml.of_string_exn configs_str in
+  let configs_list = EJ.get_dict configs_value in
+  List.map configs_list ~f:(fun (name, conf_yaml) ->
+    let clang_option = EJ.get_string (EJ.find conf_yaml [ "clang-option" ]) in
+    let discover_option =
+      EJ.get_string (EJ.find conf_yaml [ "discover-option" ]) in
+    let recurse =
+      match EJ.find_opt conf_yaml ["recursive"] with
+      | None -> false
+      | Some value ->
+        if String.equal (EJ.get_string value) "true" then true else false in
+    let targets =
+      String.split ~on:' ' (EJ.get_string (EJ.find conf_yaml [ "targets" ])) in
 
-      mk_config name clang_option discover_option recurse targets
-    )
-  with End_of_file -> [mk_default_config()]
+    mk_config name clang_option discover_option recurse targets
+  )
 
 let dir_prefix = "benchmarks/"
 let log_dir = "benchmark-log/"
 
-let rec test benchmark =
+let rec test ?(conf_file="") benchmark =
   let description, dir = benchmark in
   let full_dir = dir_prefix ^ dir in
   let _ = print ("Testing " ^ description ^ " in " ^ dir) in
-  let config_filename = full_dir ^ "/benchmarki.yaml" in
+  let config_filename = 
+    if String.equal conf_file "" then full_dir ^ "/benchmark.yaml"
+    else
+      conf_file
+  in
   let configs = read_confs_from_file config_filename in
   List.iter
     ~f:(fun conf ->
@@ -65,13 +74,21 @@ let rec test benchmark =
       let all_files = Array.to_list (Sys.readdir full_dir) in
       let _ =
         List.iter conf.conf_targets ~f:(fun target -> print ("Target: " ^ target)) in
-      (* TODO: Check if a file is a target *)
-      let is_target target_list file = true in
+      (* TODO: Do better than suffix check *)
+      let rec is_target target_list file = 
+        match target_list with
+        | [] -> false
+        | target :: tl ->
+          if String.is_suffix file ~suffix:target then true
+          else
+            is_target tl file
+        in
+      (* TODO: Make a function that runs each file *)
       let files = List.filter all_files ~f:(is_target conf.conf_targets) in
       List.iter files ~f:(fun file ->
           let full_filepath = full_dir ^ "/" ^ file in
           match Sys.is_directory full_filepath with
-          | `Yes -> test ("", full_filepath)
+          | `Yes -> test ~conf_file:config_filename ("", full_filepath)
           | `No ->
             let _ = print ("File: " ^ file) in
             let command =
