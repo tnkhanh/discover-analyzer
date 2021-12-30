12583e0c314875643d0664478cb3ae42ee10583d
TN Khanh <tnkhanh4@gmail.com>
2021-12-30 03:57:41 +0000

    Better config reading

--- src/test/test_benchmark.ml
+++ src/test/test_benchmark.ml
@@ -10,6 +10,7 @@ type config = {
   conf_clang_opt: string;
   conf_discover_opt: string;
   conf_targets: string list;
+  conf_excludes: string list;
   conf_recurse: bool;
 }
 
@@ -19,15 +20,18 @@ let default_config =
     conf_clang_opt = "";
     conf_discover_opt= "";
     conf_targets = [".c"];
+    conf_excludes = [];
     conf_recurse = false;
   }
 
-let mk_config (name: string) (clang_opt: string) (dis_opt: string) (recurse: bool) (targets: string list) =
+let mk_config (name: string) (clang_opt: string) (dis_opt: string) (recurse: bool) (excludes: string list) 
+      (targets: string list) =
   {
     conf_name = name;
     conf_clang_opt = clang_opt;
     conf_discover_opt = dis_opt;
     conf_recurse = recurse;
+    conf_excludes = excludes;
     conf_targets = targets;
   }
 
@@ -39,33 +43,41 @@ let str_of_config conf =
   "\nRecurse: " ^ (string_of_bool conf.conf_recurse)
 
 let read_confs_from_file filename =
-  let _ = print filename in
   let configs_str = 
     try In_channel.read_all filename 
-    with e -> let _ = print ("Exn: " ^ Exn.to_string e) in "" in
+    with e -> let _ = print ("Exception: " ^ Exn.to_string e) in "" in
   if String.equal configs_str "" then [default_config]
   else
-  let _ = print configs_str in
   let configs_value = Yaml.of_string_exn configs_str in
   let configs_list = EJ.get_dict configs_value in
   List.map configs_list ~f:(fun (name, conf_yaml) ->
-    let clang_option = EJ.get_string (EJ.find conf_yaml [ "clang-option" ]) in
+    let clang_option =
+      match EJ.find_opt conf_yaml [ "clang-option" ] with
+      | None -> ""
+      | Some opt -> EJ.get_string opt in
     let discover_option =
-      EJ.get_string (EJ.find conf_yaml [ "discover-option" ]) in
+      match EJ.find_opt conf_yaml [ "discover-option" ] with
+      | None -> ""
+      | Some opt -> EJ.get_string opt in
     let recurse =
       match EJ.find_opt conf_yaml ["recursive"] with
       | None -> false
-      | Some value ->
-        if String.equal (EJ.get_string value) "true" then true else false in
+      | Some opt ->
+        if String.equal (EJ.get_string opt) "yes" then true else false in
     let targets =
-      String.split ~on:' ' (EJ.get_string (EJ.find conf_yaml [ "targets" ])) in
-
-    mk_config name clang_option discover_option recurse targets
+      match EJ.find_opt conf_yaml [ "targets" ] with
+      | None -> [""]
+      | Some opt -> String.split ~on:' ' (EJ.get_string opt) in
+    let excludes =
+      match EJ.find_opt conf_yaml [ "excludes" ] with
+      | None -> []
+      | Some opt -> String.split ~on:' ' (EJ.get_string opt) in
+    mk_config name clang_option discover_option recurse excludes targets
   )
 
 let log_dir = "benchmark-log/"
 
-let rec test benchmark =
+let rec test default_conf benchmark =
   let description, dir = benchmark in
   let _ = print ("Testing " ^ description ^ " in " ^ dir) in
   let config_filename = 
@@ -73,7 +85,15 @@ let rec test benchmark =
     else
       !conf_file
   in
-  let configs = read_confs_from_file config_filename in
+  let configs = 
+    if Sys.file_exists_exn config_filename then
+      let _ = print ("Found config file: " ^ config_filename) in
+      read_confs_from_file config_filename
+    else
+      let _ = print ("Config file " ^ config_filename 
+                     ^ " not found. Using default config") in
+      [default_conf]
+  in
   List.iter
     ~f:(fun conf ->
 (*      let total_assert_ok = ref 0 in*)
@@ -83,9 +103,9 @@ let rec test benchmark =
       let total_valid_assert = ref 0 in
       let total_invalid_assert = ref 0 in
 
+      let _ = print ("Config for " ^ dir ^ ":\n" ^ (str_of_config conf)) in
       let full_log_dir = log_dir ^ dir ^ "/" ^ conf.conf_name in
       let _ = PS.run_command [ "mkdir"; "-p"; full_log_dir ] in
-      let _ = print (str_of_config conf) in
       let _ = List.iter conf.conf_targets ~f:print in
       let all_files = Array.to_list (Sys.readdir dir) in
       let _ =
@@ -99,13 +119,27 @@ let rec test benchmark =
           else
             is_target tl file
         in
-      (* TODO: Make a function that runs each file *)
-      let files = List.filter all_files ~f:(is_target conf.conf_targets) in
-      let _ = List.iter files ~f:(fun file ->
+
+      (* exclude needs to match the full file name *)
+      let rec is_included excludes file =
+        match excludes with
+        | [] -> true
+        | exclude :: tl ->
+          if String.equal file exclude then false
+          else
+            is_included tl file
+        in
+        
+      let is_test_file file = (is_target conf.conf_targets file) && (is_included conf.conf_excludes file) in
+
+      (*let test_files = List.filter all_files ~f:is_test_file in*)
+      let _ = List.iter all_files ~f:(fun file ->
           let full_filepath = dir ^ "/" ^ file in
           match Sys.is_directory full_filepath with
-          | `Yes -> test ("", full_filepath)
+          | `Yes -> if conf.conf_recurse then test conf ("rec", full_filepath) else ()
           | `No ->
+            if not (is_test_file file) then ()
+            else
             let _ = print ("File: " ^ file) in
             let command =
               [ !discover_exec; "--clang-option=" ^ conf.conf_clang_opt ]
@@ -159,7 +193,7 @@ let rec test benchmark =
             Out_channel.write_all log_file ~data:output_str
           | `Unknown -> ()) in
       let summary =
-        "Summary of assertions: " ^
+        "Summary of config " ^ conf.conf_name ^ " of benchmark " ^ dir ^ ":\n" ^
         "  Valid asssertions: " ^ (pr_int !total_valid_assert) ^
         "  Invalid asssertions: " ^ (pr_int !total_invalid_assert) in
       print summary)
@@ -181,6 +215,6 @@ let main () =
     [("-conf", Arg.Set_string conf_file, "Set config file")] in
   let _ = Arg.parse speclist anon_fun usage_msg in
   let _ = if List.is_empty !benchmarks then benchmarks := default_benchmarks else () in
-  List.iter !benchmarks ~f:test
+  List.iter !benchmarks ~f:(test default_config)
 
 let _ = main ()
