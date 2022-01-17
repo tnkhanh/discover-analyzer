de47ac097c84a90f4c77a5f9b48d3a5b5f073f1e
Ta Quang Trung <taquangtrungvn@gmail.com>
2022-01-17 12:15:56 +0800

    script to compile Solidity contracts in a directory

---
+++ scripts/compile-solidity-dir.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+## Compile all Solidity smart contracts in a directory.
+
+## Usage:
+##    compile-solidity-dir.sh <input-dir>
+
+BENCH_DIR=$1
+
+if [ -z "$BENCH_DIR" ]; then
+    echo "Please specify the input Solidity directory!"
+    echo ""
+    echo "Usage: "
+    echo "  compile-solidity-dir.sh <input-dir>"
+    exit 1;
+fi
+
+for FILE in $BENCH_DIR/*.sol; do
+    echo "+ Compiling $FILE"
+    solang --no-constant-folding --no-strength-reduce \
+           --no-dead-storage --no-vector-to-slice \
+           --no-cse --no-implicit-type-cast-check \
+           -O none --target ewasm $FILE
+    echo ""
+done
