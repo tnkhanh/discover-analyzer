f92640d43791b14ca3ec3f39881c77b9c08d06f8
Ta Quang Trung <taquangtrungvn@gmail.com>
2022-01-20 16:26:32 +0800

    minor update

--- benchmarks/smartbugs/access_control/phishable.sol
+++ benchmarks/smartbugs/access_control/phishable.sol
@@ -4,7 +4,6 @@
  * @vulnerable_at_lines: 20
  */
 
- // pragma solidity ^0.4.22;
  pragma solidity ^0.8.11;
 
  contract Phishable {
@@ -14,13 +13,11 @@
         owner = _owner;
     }
 
-    // function () public payable {} // collect ether
-    receive () external payable {} // collect ether           // TRUNG: updated to Solidity 0.8.11
+    receive () external payable {} // collect ether
 
     function withdrawAll(address _recipient) public {
         // <yes> <report> ACCESS_CONTROL
         require(tx.origin == owner);
-        // _recipient.transfer(this.balance);
-        payable(_recipient).transfer(address(this).balance);  // TRUNG: updated to Solidity 0.8.11
+        payable(_recipient).transfer(address(this).balance);
     }
 }
